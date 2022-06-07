023b51a923554cea387dc6ed97b09b2f0d31b5c8
marmola90 <amarmol20@gmail.com>
2022-06-07 10:51:58 -0600

    process env

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express= require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
 
 const db =knex({
     client: 'pg',
@@ -35,7 +35,7 @@ app.get('/profile/:id',(req,res)=> { profile.handleProfileGet(req,res,db)})
 app.put('/image',(req,res)=>{ image.handleImage(req,res,db)})
 app.post('/imageUrl',(req,res)=>{ image.handleApiCall(req,res)})
 
-app.listen(3000, ()=>{
-    console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`app is running on port ${process.env.PORT}`);
 })
 
